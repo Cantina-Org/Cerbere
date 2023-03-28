63e8bc186259360e698c6126e6507322158d79ea
mathieu <degueurce.mathieu@gmail.com>
2023-03-28 19:18:16 +0200

    Initial commit

---
+++ Cogs/home.py
@@ -0,0 +1,2 @@
+def home_cogs():
+    return 'Cantina Cerbere V0.1'
---
+++ app.py
@@ -0,0 +1,28 @@
+from json import load
+from os import path, getcwd
+from flask import Flask, request
+from Cogs.auth import auth_cogs
+from Cogs.home import home_cogs
+from Utils.Database import DataBase
+
+app = Flask(__name__)
+conf_file = open(path.abspath(getcwd()) + "/config.json", 'r')
+config_data = load(conf_file)
+
+# Connection aux bases de données
+database = DataBase(user=config_data['database'][0]['database_username'],
+                    password=config_data['database'][0]['database_password'], host="localhost", port=3306)
+
+
+@app.route('/')
+def home():
+    return home_cogs()
+
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    return auth_cogs(request, database)
+
+
+if __name__ == '__main__':
+    app.run()
