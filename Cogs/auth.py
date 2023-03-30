0f63ab562bf3ae7a0dcbe1ed6b9300005186549f
mathieu <degueurce.mathieu@gmail.com>
2023-03-30 19:18:57 +0200

    - tentative de fix de l'auth

--- Cogs/auth.py
+++ Cogs/auth.py
@@ -6,9 +6,11 @@ from flask import render_template, redirect, url_for, make_response
 def auth_cogs(ctx, database):
     if ctx.method == 'POST':
         user = ctx.form['nm']
-        passwd = ctx.form['passwd']
-        row = database.select(f'''SELECT user_name, password, token FROM cantina_administration.user WHERE password = %s 
-            AND user_name = %s''', (salt_password(passwd, user, database, ctx), user), 1)
+        try:
+            passwd = salt_password(ctx.form['passwd'], user, database, ctx)
+            row = database.select(f'''SELECT user_name, password, token FROM cantina_administration.user WHERE password = %s AND user_name = %s''', (passwd, user), 1)
+        except Exception as e:
+            return 'userNotFound: ' + str(e)
 
         try:
             make_log('login', ctx.remote_addr, row[2], 1, database)
