f025c9a50361a0d22e2ef5bd1e6ea1fb81d60618
matyu <degueurce.mathieu@gmail.com>
2023-04-05 17:57:48 +0200

    - la migration

--- Cogs/auth.py
+++ Cogs/auth.py
@@ -1,24 +1,33 @@
 from datetime import datetime
-from Utils.Utils import hash_password, make_log
+from Utils.Utils import hash_password, verify_hash_password, make_log
 from flask import render_template, redirect, url_for, make_response
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
 
 
 def auth_cogs(ctx, database):
     if ctx.method == 'POST':
         user = ctx.form['nm']
         try:
-            passwd = hash_password(ctx.form['passwd'])
-            row = database.select(f'''SELECT user_name, password, token FROM cantina_administration.user WHERE password = %s AND user_name = %s''', (passwd, user), 1)
+            passwd = ctx.form['passwd']
+            print(passwd)
+            row = database.select(f'''SELECT password, token FROM cantina_administration.user WHERE user_name = %s''', (user), 1)
+            print(row[0])
+            login = verify_hash_password(row[0], passwd)
         except Exception as e:
             return 'userNotFound: ' + str(e)
 
         try:
-            make_log('login', ctx.remote_addr, row[2], 1, database)
-            resp = make_response(redirect(url_for('home')))
-            resp.set_cookie('userID', row[2])
-            database.insert('''UPDATE cantina_administration.user SET last_online=%s WHERE token=%s''',
-                            (datetime.now(), row[2]))
-            return resp
+            if login:
+                make_log(action_name='login', user_ip=ctx.remote_addr, user_token=row[2], log_level=1, database=database())
+                resp = make_response(redirect(url_for('home')))
+                resp.set_cookie('userID', row[1])
+                database.insert('''UPDATE cantina_administration.user SET last_online=%s WHERE token=%s''',
+                                (datetime.now(), row[1]))
+                return resp
+            else:
+                return 'userNotFound'
         except Exception as error:
             make_log('Error', ctx.remote_addr, ctx.cookies.get('userID'), 2, str(error))
             return redirect(url_for("home"))
--- Utils/Utils.py
+++ Utils/Utils.py
@@ -1,10 +1,16 @@
 from argon2 import PasswordHasher
 
+from Utils import Database
+
 ph = PasswordHasher()
 
 
 def hash_password(passwordtohash: str):
     return ph.hash(passwordtohash)
+
+
+def verify_hash_password(hash, password_to_verify):
+    return ph.verify(hash, password_to_verify)
 
 
 def make_log(action_name, user_ip, user_token, log_level, database, argument=None, content=None, ):
