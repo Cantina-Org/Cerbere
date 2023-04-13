d308c7aec5c83d57b9edbc5cbcdfaa56d854fdcf
matyu <degueurce.mathieu@gmail.com>
2023-04-13 17:24:17 +0200

    - Modification de la page my_account.html

--- Cogs/my_account.py
+++ Cogs/my_account.py
@@ -1,4 +1,4 @@
-from flask import redirect, url_for
+from flask import redirect, url_for, render_template
 
 
 def my_account_cogs(ctx, database):
@@ -6,4 +6,4 @@ def my_account_cogs(ctx, database):
         return redirect(url_for('auth'))
 
     data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''', (ctx.cookies.get('userID')), 1)
-    return str(data)
+    return render_template('my_account.html', data=data)
