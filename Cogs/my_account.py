b0618e8f35d107f8d4cc66e861e506e2184c0f2c
matyu <degueurce.mathieu@gmail.com>
2023-04-18 20:06:58 +0200

    - Modification des containers dans my_account.html - Ajout de la taille du dossier data[6] dans le container 'nephelees', front et back - Création du container 'hermes', front, des datas seront à rajouter quand Hermes sera finis.

--- Cogs/my_account.py
+++ Cogs/my_account.py
@@ -1,3 +1,4 @@
+from subprocess import check_output, CalledProcessError
 from flask import redirect, url_for, render_template
 
 
@@ -5,5 +6,10 @@ def my_account_cogs(ctx, database):
     if not ctx.cookies.get('userID'):
         return redirect(url_for('auth'))
 
-    data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''', (ctx.cookies.get('userID')), 1)
-    return render_template('my_account.html', data=data)
+    data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''', (ctx.cookies.get('userID')),
+                           1)
+    try:
+        folder_size = check_output(['du', '-sh', data[6]]).split()[0].decode('utf-8')
+    except CalledProcessError:
+        folder_size = 'error'
+    return render_template('my_account.html', data=data, folder_size=folder_size)
