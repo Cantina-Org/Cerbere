29662adac67d46ad554561ce10ec5a6e512a519d
matyu <degueurce.mathieu@gmail.com>
2023-04-19 14:24:02 +0200

    - Ajout des valeurs de cantina_hermes.data dans le container hermes de my_account.html

--- Cogs/my_account.py
+++ Cogs/my_account.py
@@ -6,10 +6,12 @@ def my_account_cogs(ctx, database):
     if not ctx.cookies.get('userID'):
         return redirect(url_for('auth'))
 
-    data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''', (ctx.cookies.get('userID')),
-                           1)
+    user_data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''',
+                                (ctx.cookies.get('userID')), 1)
+    messsage_data = database.select('''SELECT * FROM cantina_hermes.data WHERE token=%s''', (ctx.cookies.get('userID'))
+                                    , 1)
     try:
-        folder_size = check_output(['du', '-sh', data[6]]).split()[0].decode('utf-8')
+        folder_size = check_output(['du', '-sh', user_data[6]]).split()[0].decode('utf-8')
     except CalledProcessError:
         folder_size = 'error'
-    return render_template('my_account.html', data=data, folder_size=folder_size)
+    return render_template('my_account.html', user_data=user_data, messsage_data=messsage_data, folder_size=folder_size)
